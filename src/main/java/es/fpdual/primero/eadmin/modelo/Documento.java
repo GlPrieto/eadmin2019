377a93ea3e51d3e8b9ce3118a8f17c44d01f9e7e
Gloria Prieto Ortega <gloriapror@gmail.com>
2019-04-12 14:29:12 +0200

    Ultima clase mybatis. 12/04

--- src/main/java/es/fpdual/primero/eadmin/modelo/Documento.java
+++ src/main/java/es/fpdual/primero/eadmin/modelo/Documento.java
@@ -1,13 +1,12 @@
 package es.fpdual.primero.eadmin.modelo;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 public class Documento extends AdministracionElectronicaBase {
 
 	private final TipoDocumento tipoDocumento;
-	
-	public Documento(int id, String nombre, Usuario usuario, LocalDate fechaCreacion, TipoDocumento tipoDocumento) {
+
+	public Documento(Integer id, String nombre, Usuario usuario, LocalDate fechaCreacion, TipoDocumento tipoDocumento) {
 		this.id = id;
 		this.nombre = nombre;
 		this.usuario = usuario;
@@ -15,12 +14,16 @@ public class Documento extends AdministracionElectronicaBase {
 		this.tipoDocumento = tipoDocumento;
 	}
 
+	public Documento() {
+		this(null, null, null, null, null);
+	}
+
 	public TipoDocumento getTipoDocumento() {
 		return tipoDocumento;
 	}
-	
+
 	@Override
-	public boolean equals (Object obj) {
+	public boolean equals(Object obj) {
 		if (obj instanceof Documento) {
 			final Documento documento = (Documento) obj;
 			return documento.getId() == this.id;
@@ -33,5 +36,63 @@ public class Documento extends AdministracionElectronicaBase {
 		return "Documento " + id + ", nombre: " + nombre;
 	}
 
-	
+	public static DocumentoBuilder builder() {
+		return Documento.DocumentoBuilder.builder();
+	}
+
+	public static class DocumentoBuilder {
+
+		private int id;
+
+		private String nombre;
+
+		private Usuario usuario;
+
+		private LocalDate fechaCreacion;
+
+		private TipoDocumento tipoDocumento;
+
+		public static DocumentoBuilder builder() {
+			return new DocumentoBuilder();
+		}
+
+		public Documento build() {
+			return new Documento(this.id, this.nombre, this.usuario, this.fechaCreacion, this.tipoDocumento);
+		}
+
+		public DocumentoBuilder clone(Documento documento) {
+			this.id = documento.getId();
+			this.nombre = documento.getNombre();
+			this.usuario = documento.getUsuario();
+			this.fechaCreacion = documento.getFechaCreacion();
+			this.tipoDocumento = documento.getTipoDocumento();
+			return this;
+		}
+
+		public DocumentoBuilder withId(int id) {
+			this.id = id;
+			return this;
+		}
+
+		public DocumentoBuilder withNombre(String nombre) {
+			this.nombre = nombre;
+			return this;
+		}
+
+		public DocumentoBuilder withUsuario(Usuario usuario) {
+			this.usuario = usuario;
+			return this;
+		}
+
+		public DocumentoBuilder withFechaCreacion(LocalDate fechaCreacion) {
+			this.fechaCreacion = fechaCreacion;
+			return this;
+		}
+
+		public DocumentoBuilder withTipoDocumento(TipoDocumento tipoDocumento) {
+			this.tipoDocumento = tipoDocumento;
+			return this;
+		}
+	}
+
 }
--- src/test/java/es/fpdual/primero/eadmin/mapper/BaseDocumentoMapperTest.java
+++ src/test/java/es/fpdual/primero/eadmin/mapper/BaseDocumentoMapperTest.java
@@ -1,32 +1,76 @@
 package es.fpdual.primero.eadmin.mapper;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import java.time.LocalDate;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import es.fpdual.primero.eadmin.modelo.Documento;
 import es.fpdual.primero.eadmin.modelo.TipoDocumento;
 import es.fpdual.primero.eadmin.modelo.Usuario;
 
+@Transactional("eadminTransactionManager")
 public abstract class BaseDocumentoMapperTest {
+
+	private Usuario usuario;
+	private LocalDate fecha;
+	private Documento documento;
 
 	@Autowired
 	DocumentoMapper mapper;
 
+	@Before
+	public void inicializarEnCadaTest() {
+		this.usuario = new Usuario(1, "Manu", "Master");
+		this.fecha = LocalDate.now();
+		this.documento = new Documento(1, "Documento de prueba", usuario, fecha, TipoDocumento.DOCUMENTO_PADRON);
+	}
+
 	@Test
 	public void deberiaInsertarUnDocumento() throws Exception {
 		// DECLARACION
-		Usuario usuario = new Usuario(1, "Manu", "Master");
-		LocalDate fecha = LocalDate.now();
-		Documento documento = new Documento(1, "Documento de prueba", usuario, fecha, TipoDocumento.DOCUMENTO_PADRON);
+
 		// ENTRENAMIENTO
+
 		// PRUEBA
 		Integer resultado = this.mapper.insertarDocumento(documento);
 		// COMPROBACION
 		assertThat(resultado, is(1));
+	}
+
+	@Test
+	public void deberiaRescatarElDocumento() throws Exception {
+		// DECLARACION
+
+		// ENTRENAMIENTO
+		this.mapper.insertarDocumento(this.documento);
+		// PRUEBA
+		Documento resultado = this.mapper.getDocumento(1);
+		// COMPROBACION
+		assertThat(resultado, is(this.documento));
+	}
+
+	@Test
+	public void deberiaModificarElDocumento() throws Exception {
+		// DECLARACIÓN
+		Documento modificado = Documento.builder().clone(this.documento).withNombre("nombre actualizado").build();
+
+		// ENTRENAMIENTO
+		this.mapper.insertarDocumento(this.documento);
+
+		// PRUEBA
+		Integer resultado = this.mapper.actualizarDocumento(modificado);
+
+		// COMPROBACIÓN
+		assertThat(resultado, is(1));
+		Documento documentoActualizado = this.mapper.getDocumento(1);
+		assertThat(documentoActualizado, is(notNullValue()));
+		assertThat(documentoActualizado.getNombre(), is("nombre actualizado"));
 	}
 }
